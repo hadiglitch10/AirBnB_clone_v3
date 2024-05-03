1998fddc54d003028359bdeb0f74c1dfda2e5078
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2024-05-03 18:16:06 +0300

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,11 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flasgger import Swagger 
+from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
 
 
 @app.teardown_appcontext
@@ -20,15 +19,8 @@ def teardown(self):
     """Remove the current SQLAlchemy session"""
     return storage.close()
 
-if __name__ == "__main__":
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    else:
-        "0.0.0.0"
-    
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
-    else:
-        5000
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
-    
